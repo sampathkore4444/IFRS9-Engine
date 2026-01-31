980622c43ac33db77fdb5efb0eb0490e0496bb0a
Kore Sampath Kumar <sampathkore4444@gmail.comr>
2026-01-31 12:30:19 +0700

    modified the code for both ead and without ead modelling

--- run_ifrs9.py
+++ run_ifrs9.py
@@ -7,8 +7,8 @@ from models.lgd_cashflow import lgd_cashflow
 from models.ead_amortization import build_ead_schedule
 from models.ead_ccf import calculate_ead_ccf
 
-from engine.ecl_monthly import monthly_ecl
-
+from engine.ecl_monthly import monthly_ecl_with_ead_model
+from engine.ecl_monthly import monthly_ecl_without_ead_model
 import ast
 
 
@@ -79,14 +79,21 @@ for _, row in loans.iterrows():
             * s["lgd_multiplier"]
         )
 
-        ecl = monthly_ecl(
+        ecl = monthly_ecl_without_ead_model(
             pd_term_structure=pd_ts,
             lgd=lgd,
-            # ead=row["ead"], without modelling ead
-            ead_term_structure=ead_ts,  # With modelling ead
+            ead=row["ead"],  # without modelling ead
             eir=row["eir"],
             stage=stage,
         )
+
+        # ecl = monthly_ecl_with_ead_model(
+        #     pd_term_structure=pd_ts,
+        #     lgd=lgd,
+        #     ead_term_structure=ead_ts,  # With modelling ead
+        #     eir=row["eir"],
+        #     stage=stage,
+        # )
 
         ecl_total += s["weight"] * ecl
 
